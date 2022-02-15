1dc0444a778b3790917fa1d21430aac2eda99b5a
yushan <yushanwebdev@gmail.com>
2022-02-15 16:05:43 +0530

    Section 02 - Adding Your First React Leaflet Map to a New React Application - Install Leaflet and react-leaflet with npm in a JavaScript Project - Create a New Map in React Leaflet using the Components TileLayer and Map - Style React Leaflet Components using CSS

--- lessons/02 - Adding Your First React Leaflet Map to a New React Application/src/App.js
+++ lessons/02 - Adding Your First React Leaflet Map to a New React Application/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import './assets/stylesheets/App.css';
+import React from "react";
+import "./assets/stylesheets/App.css";
 
 /**
  * @lesson-02-todo Exercise 2
  * In order to build our map, we need to import out components.
  * How can we add those to our project?
  */
+import { Map, TileLayer } from "react-leaflet";
 
-
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
     <Layout>
-      { /**
+      {/**
        * @lesson-02-todo Exercise 2
        * We want to add our first map to the app. Using the
        * the components we just imported above, how can we
        * configure the props to show our map?
-       */ }
-      <h1 className="text-center">Welcome to LaunchTime</h1>
+       */}
+      <Map center={[38.907132, -77.036546]} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
     </Layout>
   );
 }
