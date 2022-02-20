77451b06298e261a5e3652cdf05adbce0afb453d
yushan <yushanwebdev@gmail.com>
2022-02-20 10:51:16 +0530

    Section 3 - Customize a React Leaflet Map with a Mapbox Basemap Style - Create a Mapbox Account - Create a Map Style in Mapbox Studio - Create an API Key in Mapbox - Extract Enviornment Variable for the API - Configure a Mapbox Endpoint with Mapbox GET Request Veriables - Customize React Leaflet Maps with a Custom Map Style Endpoint - Extract Enviornment Variables to a .env File in a JavaScript Project - Create a New Basemap Style in Mapbox Studio

--- lessons/03 - Customizing Your React Leaflet Map with a Mapbox Basemap Style/src/App.js
+++ lessons/03 - Customizing Your React Leaflet Map with a Mapbox Basemap Style/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React from "react";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Layout>
-      <Map center={[38.907132, -77.036546]} zoom={12}>
-      { /**
+      <Map center={[6.9218386, 79.8562055]} zoom={25}>
+        {/**
          * @lesson-03-todo
          * Now that we created a new Map Style with Mapbox,
          * we want to use it in our app so that we can have
          * a custom map. To do that, we'll need to update our
          * TileLayer to use a Mapbox endpoint and attribution.
-         */ }
+         */}
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERID}/${process.env.REACT_APP_MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
+          attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
         />
       </Map>
     </Layout>
