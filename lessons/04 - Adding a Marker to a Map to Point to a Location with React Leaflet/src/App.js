814d2f017b443c19ee9cae46b166122ab5337ea2
yushan <yushanwebdev@gmail.com>
2022-02-27 13:39:18 +0530

    Section 4 - Adding a Marker Component to a Map to Point to a Location with React Leaflet - Retrieve Latitude and Longitude Coordinates with Google Maps - Add a React Leaflet Marker Component with Latitude and Longitude Coordinates - Fix react-leaflet Peer Dependencies Conflicts with useEffect Hook - Add a React Leaflet Popup Component to Display the Name of an Existing Marker Component - Add Latitude and Longitude Coordinates to a React Leaflet Marker Component

--- lessons/04 - Adding a Marker to a Map to Point to a Location with React Leaflet/src/App.js
+++ lessons/04 - Adding a Marker to a Map to Point to a Location with React Leaflet/src/App.js
@@ -5,16 +5,26 @@
  * those components. How can we import them below?
  */
 
-import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React from "react";
+import L from "leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
+
+import Pin from "leaflet/dist/images/marker-icon.png";
 
 const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
 const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
 const MAPBOX_STYLEID = process.env.REACT_APP_MAPBOX_STYLEID;
+
+const myIcon = L.icon({
+  iconUrl: Pin,
+  iconSize: [25, 41],
+  iconAnchor: [18, 26],
+  popupAnchor: [-5, -8],
+});
 
 function App() {
   /**
@@ -27,17 +37,23 @@ function App() {
 
   return (
     <Layout>
-      <Map center={[38.907132, -77.036546]} zoom={12}>
+      <Map center={[6.917794058910394, 79.8551501685462]} zoom={30}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
-        { /**
-           * @lesson-04-todo
-           * We want to show our website visitors our favorite
-           * local attraction. How can we make use of the Leaflet
-           * Marker and Popup component to point our location out?
-           */ }
+        {/**
+         * @lesson-04-todo
+         * We want to show our website visitors our favorite
+         * local attraction. How can we make use of the Leaflet
+         * Marker and Popup component to point our location out?
+         */}
+        <Marker position={[6.917794058910394, 79.8551501685462]} icon={myIcon}>
+          <Popup>Colombo City Center</Popup>
+        </Marker>
+        <Marker position={[6.918053872585305, 79.85510894292193]} icon={myIcon}>
+          <Popup>Bubble Me Bubble Tea - Colombo City Center</Popup>
+        </Marker>
       </Map>
     </Layout>
   );
