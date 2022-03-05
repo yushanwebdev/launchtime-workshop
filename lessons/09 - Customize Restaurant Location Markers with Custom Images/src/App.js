d1fb7d5cdf02b1c2d67fa4a67b1f0840c3476eee
yushan <yushanwebdev@gmail.com>
2022-03-05 17:36:08 +0530

    Section 9 - Customize Restaurant Location Markers with Custom Images - Recreate React Leaflet Markers with the GeoJSON pointToLayer Configuration Set - Replace the Default React Leaflet Location Markers with a Custom Image - Import and Add the Default React Leaflet Shadows back to our Markers - Replace the Marker with Custom HTML and Style with CSS

--- lessons/09 - Customize Restaurant Location Markers with Custom Images/src/App.js
+++ lessons/09 - Customize Restaurant Location Markers with Custom Images/src/App.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
-import locations from './data/locations';
+import locations from "./data/locations";
+
+// import utensilsIcon from "./assets/shared/utensils-marker.png";
+// import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 /**
  * @lesson-09-todo
@@ -25,26 +28,25 @@ function App() {
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require( 'leaflet/dist/images/marker-icon-2x.png' ),
-      iconUrl: require( 'leaflet/dist/images/marker-icon.png' ),
-      shadowUrl: require( 'leaflet/dist/images/marker-shadow.png' ),
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
     });
   }, []);
 
   useEffect(() => {
-
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
-    if ( !map ) return;
+    if (!map) return;
 
     map.eachLayer((layer = {}) => {
       const { options } = layer;
       const { name } = options;
 
-      if ( name !== 'Mapbox' ) {
+      if (name !== "Mapbox") {
         map.removeLayer(layer);
-      };
+      }
     });
 
     const geoJson = new L.GeoJSON(locations, {
@@ -54,17 +56,38 @@ function App() {
        * to customize our location data. How can we use the options available
        * to us to add a custom image for our markers?
        */
-
+      /* Using Image Icon */
+      // pointToLayer: (feature, latlng) => {
+      //   return L.marker(latlng, {
+      //     icon: new L.Icon({
+      //       iconUrl: utensilsIcon,
+      //       iconSize: [26, 26],
+      //       popupAnchor: [0, -15],
+      //       shadowUrl: markerShadow,
+      //       shadowAnchor: [13, 28],
+      //     }),
+      //   });
+      // },
+      /* Using Div Icon */
+      pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            html: "<div class='location-marker'>🍽️</div>",
+            iconSize: [26, 26],
+          }),
+        });
+      },
       onEachFeature: (feature = {}, layer) => {
-        const { properties = {}, geometry = {}  } = feature;
-        const { name, delivery, deliveryRadius, tags, phone, website } = properties;
+        const { properties = {}, geometry = {} } = feature;
+        const { name, delivery, deliveryRadius, tags, phone, website } =
+          properties;
         const { coordinates } = geometry;
 
         let deliveryZoneCircle;
 
-        if ( deliveryRadius ) {
+        if (deliveryRadius) {
           deliveryZoneCircle = L.circle(coordinates.reverse(), {
-            radius: deliveryRadius
+            radius: deliveryRadius,
           });
         }
 
@@ -75,10 +98,10 @@ function App() {
             <h3>${name}</h3>
             <ul>
               <li>
-                ${tags.join(', ')}
+                ${tags.join(", ")}
               </li>
               <li>
-                <strong>Delivery:</strong> ${delivery ? 'Yes' : 'No'}
+                <strong>Delivery:</strong> ${delivery ? "Yes" : "No"}
               </li>
               <li>
                 <strong>Phone:</strong> ${phone}
@@ -90,22 +113,22 @@ function App() {
           </div>
         `;
 
-        popup.setContent(html)
+        popup.setContent(html);
 
         layer.bindPopup(popup);
 
-        layer.on('mouseover', () => {
-          if ( deliveryZoneCircle ) {
+        layer.on("mouseover", () => {
+          if (deliveryZoneCircle) {
             deliveryZoneCircle.addTo(map);
           }
         });
 
-        layer.on('mouseout', () => {
-          if ( deliveryZoneCircle ) {
+        layer.on("mouseout", () => {
+          if (deliveryZoneCircle) {
             deliveryZoneCircle.removeFrom(map);
           }
         });
-      }
+      },
     });
 
     geoJson.addTo(map);
@@ -116,7 +139,7 @@ function App() {
       <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
       </Map>
     </Layout>
