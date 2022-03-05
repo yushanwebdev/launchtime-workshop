7c12e73406b726630c26ac283ed2d189ccbb7821
yushan <yushanwebdev@gmail.com>
2022-03-05 14:19:52 +0530

    Section 7 - Add Restaurant Info to GeoJSON Documents and Display it in a Tooltip on the Map - Update our GeoJSON Data to Include Restaurant Information by Adding properties - Add React Leaflet Popups to all of our Markers using GeoJSON Data - Adding Restaurant Information to our Popups - Update the Styles of React Leaflet Popups - Change the Background Color of a React Leaflet Popup with CSS Selectors - Add Another Restaurant Attribute with a GeoJSON Property and Display it in a Leaflet Popup

--- lessons/07 - Add Restaurant Info to Your GeoJSON Document and Display It in a Tooltip on the Map/src/App.js
+++ lessons/07 - Add Restaurant Info to Your GeoJSON Document and Display It in a Tooltip on the Map/src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
-import locations from './data/locations';
+import locations from "./data/locations";
 
 const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
 const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
@@ -19,26 +19,25 @@ function App() {
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require( 'leaflet/dist/images/marker-icon-2x.png' ),
-      iconUrl: require( 'leaflet/dist/images/marker-icon.png' ),
-      shadowUrl: require( 'leaflet/dist/images/marker-shadow.png' ),
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
     });
   }, []);
 
   useEffect(() => {
-
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
-    if ( !map ) return;
+    if (!map) return;
 
     map.eachLayer((layer = {}) => {
       const { options } = layer;
       const { name } = options;
 
-      if ( name !== 'Mapbox' ) {
+      if (name !== "Mapbox") {
         map.removeLayer(layer);
-      };
+      }
     });
 
     /**
@@ -50,7 +49,40 @@ function App() {
      * What option can we use to add popups to our markers?
      */
 
-    const geoJson = new L.GeoJSON(locations);
+    const geoJson = new L.GeoJSON(locations, {
+      onEachFeature: (feature = {}, layer) => {
+        const { properties = {} } = feature;
+        const { name, delivery, tags, phone, website, vegan } = properties;
+
+        const popup = L.popup();
+
+        const html = `
+          <div class="restaurant-popup">
+            <h3>${name}</h3>
+            <ul>
+              <li>
+                ${tags.join(", ")}
+              </li>
+              <li>
+                <strong>Delivery:</strong> ${delivery ? "Yes" : "No"}
+              </li>
+              <li>
+                <strong>Phone:</strong> ${phone}
+              </li>
+              <li>
+                <strong>Webiste:</strong> <a href="${website}">${website}</a>
+              </li>
+              <li>
+                <strong>Vegan Friendly?:</strong> ${vegan ? "Yes" : "No"}
+              </li>
+            </ul>
+          </div>
+        `;
+
+        popup.setContent(html);
+        layer.bindPopup(popup);
+      },
+    });
 
     geoJson.addTo(map);
   }, [mapRef]);
@@ -60,7 +92,7 @@ function App() {
       <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
       </Map>
     </Layout>
