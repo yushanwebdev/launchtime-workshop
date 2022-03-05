a3166a59d200522dd61fe80cc978f405f51e21bb
yushan <yushanwebdev@gmail.com>
2022-03-05 13:39:21 +0530

    Section 6 - Create Your First GeoJSON Document and Add Restaurant Locations to the Map - Understanding the Basics of the GeoJSON standard - Drop points in geojson.io to Create Your First GeoJSON FeatureCollection - Manually Add a New Restaurant Location Feature to the GeoJSON Document - Create a New GeoJSON File and Import it into a React App - Add GeoJSON Location Data to a React Leaflet Map with a GeoJSON Instance - Manually Add Another GeoJSON Location Feature to a Map with React Leaflet

--- lessons/06 - Create Your First GeoJSON Document and Add Your Favorite Restaurant Locations to the Map/src/App.js
+++ lessons/06 - Create Your First GeoJSON Document and Add Your Favorite Restaurant Locations to the Map/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
+import locations from "./data/locations.json";
 
 /**
  * @lesson-06-todo
@@ -23,26 +24,25 @@ function App() {
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require( 'leaflet/dist/images/marker-icon-2x.png' ),
-      iconUrl: require( 'leaflet/dist/images/marker-icon.png' ),
-      shadowUrl: require( 'leaflet/dist/images/marker-shadow.png' ),
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
     });
   }, []);
 
   useEffect(() => {
-
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
-    if ( !map ) return;
+    if (!map) return;
 
     map.eachLayer((layer = {}) => {
       const { options } = layer;
       const { name } = options;
 
-      if ( name !== 'Mapbox' ) {
+      if (name !== "Mapbox") {
         map.removeLayer(layer);
-      };
+      }
     });
 
     /**
@@ -51,11 +51,9 @@ function App() {
      * document ready to add to our Leaflet map. How can we use
      * the Leaflet GeoJSON instance to add or locations?
      */
+    const geoJson = new L.GeoJSON(locations);
 
-    const marker = L.marker([38.888369, -77.019900])
-
-    marker.bindPopup('Smithsonian National Air and Space Museum');
-    marker.addTo(map);
+    geoJson.addTo(map);
   }, [mapRef]);
 
   return (
@@ -63,7 +61,7 @@ function App() {
       <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
       </Map>
     </Layout>
