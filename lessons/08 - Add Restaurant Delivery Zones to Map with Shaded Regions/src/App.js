2ebb073fc207519d46b74ff487e44a51c299cfc7
yushan <yushanwebdev@gmail.com>
2022-03-05 16:10:36 +0530

    Section 8 - Add Restaurant Delivery Zones to Map with Shaded Regions - Add a Delivery Radius to our Restaurant GeoJSON Data - Only Showing the Delivery Radius in React Leaflet on Marker Hover using DOM Events - Change the Color of the React Leaflet Delivery Zone

--- lessons/08 - Add Restaurant Delivery Zones to Map with Shaded Regions/src/App.js
+++ lessons/08 - Add Restaurant Delivery Zones to Map with Shaded Regions/src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './assets/stylesheets/App.css';
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./assets/stylesheets/App.css";
 
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
-import locations from './data/locations';
+import locations from "./data/locations";
 
 const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
 const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
@@ -19,26 +19,25 @@ function App() {
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require( 'leaflet/dist/images/marker-icon-2x.png' ),
-      iconUrl: require( 'leaflet/dist/images/marker-icon.png' ),
-      shadowUrl: require( 'leaflet/dist/images/marker-shadow.png' ),
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
     });
   }, []);
 
   useEffect(() => {
-
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
-    if ( !map ) return;
+    if (!map) return;
 
     map.eachLayer((layer = {}) => {
       const { options } = layer;
       const { name } = options;
 
-      if ( name !== 'Mapbox' ) {
+      if (name !== "Mapbox") {
         map.removeLayer(layer);
-      };
+      }
     });
 
     const geoJson = new L.GeoJSON(locations, {
@@ -50,8 +49,19 @@ function App() {
          * file. How can we use it to show delivery zones on the map?
          */
 
-        const { properties = {} } = feature;
-        const { name, delivery, tags, phone, website } = properties;
+        const { properties = {}, geometry = {} } = feature;
+        const { name, delivery, deliveryRadius, tags, phone, website } =
+          properties;
+        const { coordinates } = geometry;
+
+        let deliveryZoneCircle;
+
+        if (delivery) {
+          deliveryZoneCircle = L.circle(coordinates.reverse(), {
+            radius: deliveryRadius,
+            color: "blueviolet",
+          });
+        }
 
         const popup = L.popup();
 
@@ -60,10 +70,10 @@ function App() {
             <h3>${name}</h3>
             <ul>
               <li>
-                ${tags.join(', ')}
+                ${tags.join(", ")}
               </li>
               <li>
-                <strong>Delivery:</strong> ${delivery ? 'Yes' : 'No'}
+                <strong>Delivery:</strong> ${delivery ? "Yes" : "No"}
               </li>
               <li>
                 <strong>Phone:</strong> ${phone}
@@ -75,10 +85,22 @@ function App() {
           </div>
         `;
 
-        popup.setContent(html)
+        popup.setContent(html);
 
         layer.bindPopup(popup);
-      }
+
+        layer.on("mouseover", () => {
+          if (deliveryZoneCircle) {
+            deliveryZoneCircle.addTo(map);
+          }
+        });
+
+        layer.on("mouseout", () => {
+          if (deliveryZoneCircle) {
+            deliveryZoneCircle.removeFrom(map);
+          }
+        });
+      },
     });
 
     geoJson.addTo(map);
@@ -89,7 +111,7 @@ function App() {
       <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
         />
       </Map>
     </Layout>
